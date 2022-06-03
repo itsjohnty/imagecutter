eef7dc3e716985decf33bf461e3aa1363e3fcab4
itsjohnty <rhodzjohnty@gmail.com>
2022-06-03 21:52:20 +0530

    folders

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css'
+
+import ImgDropAndCrop from './component/ImgDropAndCrop'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <ImgDropAndCrop />
+      </div>
+    )
+  }
+}
+
+export default App
